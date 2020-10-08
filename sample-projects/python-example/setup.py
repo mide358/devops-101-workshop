6f0f6f8d9eb189349a10969a393e7de571d98807
Batel Tova <bately77@gmail.com>
2020-10-08 23:52:47 +0300

    Update setup.py

--- sample-projects/python-example/setup.py
+++ sample-projects/python-example/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sample-pkg-katcosgrove", # Replace with your own name
+    name="sample-pkg-batelz", # Replace with your own name
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
